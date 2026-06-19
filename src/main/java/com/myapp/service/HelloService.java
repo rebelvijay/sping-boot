0815328413aba7e97113d287b925598c192f9f2e
rebelvijay <88149700+rebelvijay@users.noreply.github.com>
2026-06-19 13:34:18 +0530

    Update HelloService.java

--- src/main/java/com/myapp/service/HelloService.java
+++ src/main/java/com/myapp/service/HelloService.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public class HelloService {
 
     public String getMessage() {
-        return "Hello kotikilapudi";
+        return "Hello Nuka vijay";
     }
 }
